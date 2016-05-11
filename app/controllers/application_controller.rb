694b929d19e9aa7b42ca4d973be5f80c689f1df2
Skye <skye.weir@gmail.com>
2016-05-11 00:52:32 -0700

    current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   before_action :authenticate_user!
 
   realtime_controller({:queue => :redis}) # instruct all requests to enable realtime support via redis
 
   def realtime_user_id
-    return 42 # if using devise, change this to current_user.id
+    return current_user.id if current_user
   end
 
   def realtime_server_url
