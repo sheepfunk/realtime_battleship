4fe96eba6b574ff989f1fcff736ac96ed1a77f44
Skye <skye.weir@gmail.com>
2016-05-17 16:21:00 -0700

    message handling in the angular controller and overriding the message handler in the gem

--- app/assets/javascripts/angular-app/controllers/moveCtrl.js
+++ app/assets/javascripts/angular-app/controllers/moveCtrl.js
@@ -2,7 +2,6 @@ angular.module('app.battleshipApp').controller("MoveCtrl", ['$scope', '$window',
 
     $scope.game;
     $scope.welcome = "Hi Sailor!";
-    $scope.$log = $log;
 
     $scope.init = function(playerId, gameId)
     {
@@ -19,36 +18,37 @@ angular.module('app.battleshipApp').controller("MoveCtrl", ['$scope', '$window',
                 console.log("Failed to get the game, result is " + result);
             }
         );
+        $scope.initMessageHandler();
     };
 
-    // https://github.com/mikeatlas/realtime-rails/blob/master/app/views/realtime/_realtime_message_console_logger.html.erb
-    debugger;
-    if ($window.realtime.enabled && $window.realtime.eventBus){
-        // handle events in the queue with eventing
-        var realtimeMessageEventConsoleLogger = function(message) {
+    $scope.initMessageHandler = function() {
+        // https://github.com/mikeatlas/realtime-rails/blob/master/app/views/realtime/_realtime_message_console_logger.html.erb
+        if ($window.realtime.enabled && $window.realtime.eventBus) {
+            // handle events in the queue with eventing
+            var realtimeMessageEventConsoleLogger = function (message) {
 
-            $scope.message = "Hi";
-            $scope.message = message;
-            $scope.message = "Muffin";
-        };
-        $window.realtime.eventBus.on('realtimeMessage', realtimeMessageEventConsoleLogger);
+                $log.log("Hi");
+                $log.log(message);
+                $log.log("Muffin");
+            };
+            $window.realtime.eventBus.on('realtimeMessage', realtimeMessageEventConsoleLogger);
 
-    } else if ($window.realtime.enabled) {
-        // handle events in the queue without eventing
-        messageQueueConsoleLogger = function() {
-            message = window.realtime.messageQueue.shift();
-            if (message) {
-                $scope.message = "Hi";
-                $scope.message = message;
-                $scope.message = "Muffin";
-            }
-        };
-        setInterval(messageQueueConsoleLogger, 100);
+        } else if ($window.realtime.enabled) {
+            // handle events in the queue without eventing
+            messageQueueConsoleLogger = function () {
+                message = window.realtime.messageQueue.shift();
+                if (message) {
+                    $log.log("Hi");
+                    $log.log(message);
+                    $log.log("Muffin");
+                }
+            };
+            setInterval(messageQueueConsoleLogger, 100);
 
-    } else {
-        console.log('Error: Realtime was not enabled.')
+        } else {
+            $log.log('Error: Realtime was not enabled.')
+        }
     }
-
 
 
 
